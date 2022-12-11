0056e5609068c9aba440f288320d0b15373285fa
Gorka Diez <99983319+godidev@users.noreply.github.com>
2022-12-11 14:05:14 +0100

    adds a new person

---
+++ part2/phoneguide/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+    { name: 'Perico palotes' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    setPersons(persons.concat({ name: newName }))
+    setNewName('')
+  }
+
+  function handleNameChange(event) {
+    const { value } = event.target
+    setNewName(value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input onChange={handleNameChange} value={newName} />
+        </div>
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(({ name }) =>
+        <p key={name}>{name}</p>
+      )}
+    </div>
+  )
+}
+
+export default App
