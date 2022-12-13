ea6e026fdc2ff72b96fa162d763378b066e166ea
Gorka Diez <99983319+godidev@users.noreply.github.com>
2022-12-13 11:32:59 +0100

    shows single country, needs refreshing

--- part2/countries/src/services/useCountries.js
+++ part2/countries/src/services/useCountries.js
@@ -1,11 +1,33 @@
 import Country from "../components/Country"
+import { useState } from 'react'
 
-export default function useCountries(countries){
+export default function useCountries(countries) {
+    const [showCountry, setShowCountry] = useState(countries)
+    function handleClick(name) {
+        setShowCountry(countries.filter(country => country.name.common === name))
+        console.log(showCountry);
+    }
+
+    if (showCountry.length === 1) {
+        return showCountry.map(country => {
+            const { area = '', capital = '', languages = '' } = country
+            return (
+                <Country
+                    key={capital + area}
+                    area={area}
+                    name={country.name.common}
+                    capital={capital}
+                    flag={country.flags.png}
+                    languages={Object.values(languages).map(lang => lang)}
+                />
+            )
+        })
+    }
     if (!countries.length || countries.length === 0) {
         return <p>Nothing to search</p>
     } else {
         if (countries.length === 1) {
-           return countries.map(country => {
+            return countries.map(country => {
                 const { area = '', capital = '', languages = '' } = country
                 return (
                     <Country
@@ -24,9 +46,13 @@ export default function useCountries(countries){
             return countries.map((country, index) => {
                 const { population } = country
                 return (
-                    <p key={index + population}>
-                        {country.name.common}
-                    </p>
+                    <div key={index + population}>
+                        <span>
+                            {country.name.common}
+                        </span>
+                        <button onClick={() => handleClick(country.name.common)}>Show</button>
+                    </div>
+
                 )
             })
         }
