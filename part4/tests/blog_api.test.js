346545aabcbc89db93415ab51d2adcfaf8435493
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-01-09 15:46:03 +0100

    feat: add beforeEach to control data

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,8 +1,34 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+const blogs = [
+  {
+    title: 'this is blog 1',
+    author: 'godidev',
+    url: 'www.asdf.com',
+    likes: 288
+  },
+  {
+    title: 'this is blog 2',
+    author: 'facundo',
+    url: 'www.qwerty.com',
+    likes: 18
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blog1 = new Blog(blogs[0])
+  await blog1.save()
+
+  const blog2 = new Blog(blogs[1])
+  await blog2.save()
+})
 
 test('blogs are returned as json', async () => {
   await api
