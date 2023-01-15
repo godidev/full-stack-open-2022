7d639875d7e0671474085d621cd8b1b26ffcc950
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-01-15 18:46:59 +0100

    feat: add login Routes

--- part4/app.js
+++ part4/app.js
@@ -4,12 +4,15 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require ('./controllers/login')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true)
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/login', loginRouter)
 
 const mongoUrl = config.MONGODB_URI
 
