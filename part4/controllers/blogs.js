8e0341bd85e4c84c47e44710abf6874d5634dd22
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-01-16 22:09:58 +0100

    feat: add token functionallity creating blogs

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user')
@@ -12,9 +21,15 @@ blogsRouter.post('/', async (request, response) => {
   if (!body.title || !body.url) {
     response.status(400).end()
   } else {
-    const { title, author, url, likes = 0, userId } = body
+    const { title, author, url, likes = 0 } = body
 
-    const user = await User.findById(userId)
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
       title,
