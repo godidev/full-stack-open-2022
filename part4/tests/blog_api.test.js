f6b196bb88aa5de64d5514ed908af9912e2260a0
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-01-17 20:01:00 +0100

    feat: tests up to date

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,105 +1,172 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
-const { getOneUserId } = require('./helpers')
-const server = require('../index')
+const helper = require('./helpers')
 
 const api = supertest(app)
 
-const blogs = [
-  {
-    title: 'this is blog 1',
-    author: 'godidev',
-    url: 'www.asdf.com',
-    likes: 288
-  },
-  {
-    title: 'this is blog 2',
-    author: 'facundo',
-    url: 'www.qwerty.com',
-    likes: 18
-  }
-]
+let token
+let noBlogsToken
 
 beforeEach(async () => {
   await Blog.deleteMany({})
   await User.deleteMany({})
 
-  const user = new User({ username: 'enigma69', name: 'Pedro', password: 'rotonda' })
-  await user.save()
+  const rootUser = await new User({
+    username: 'root',
+    passowrd: 'secret',
+  }).save()
 
-  const blog1 = new Blog(blogs[0])
-  await blog1.save()
+  const userWithNoBlogs = await new User({
+    username: 'notRoot',
+    passowrd: 'aLittleLessSecret',
+  }).save()
 
-  const blog2 = new Blog(blogs[1])
-  await blog2.save()
+  const userForToken = { username: rootUser.username, id: rootUser.id }
+  token = jwt.sign(userForToken, process.env.SECRET)
+
+  const userWithNoBlogsToken = {
+    username: userWithNoBlogs.username,
+    id: userWithNoBlogs.id,
+  }
+  noBlogsToken = jwt.sign(userWithNoBlogsToken, process.env.SECRET)
+
+  await Promise.all(
+    helper.initialBlogs.map((blog) => {
+      blog.user = rootUser.id
+      return new Blog(blog).save()
+    })
+  )
 })
 
-afterEach(() => { server.close() })
+describe('Get info from blogs', () => {
+  test('can be viewed individually', async () => {
+    const allBlogsInDb = await helper.blogsInDb()
+    const blogToView = allBlogsInDb[0]
 
-describe('Get data from database', () => {
-  test('blogs are returned as json', async () => {
-    await api
-      .get('/api/blogs')
+    const response = await api
+      .get(`/api/blogs/${blogToView.id}`)
       .expect(200)
       .expect('Content-Type', /application\/json/)
-  })
 
-  test('Is ID field defined as `id` instead of `_id`', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body[0].id).toBeDefined()
+    expect(response.body.id).toBe(blogToView.id)
   })
 })
 
 describe('Add blogs to database', () => {
+  test('cannot be added without a valid user token', async () => {
+    const newBlog = {
+      title: 'Just an example blog title',
+      author: 'John Doe',
+      url: 'http://www.example.com',
+      likes: 12,
+    }
+
+    await api.post('/api/blogs').send(newBlog).expect(401)
+  }
+  )
+
+  test('can be added with a valid user token', async () => {
+    const newBlog = {
+      title: 'Just an example blog title',
+      author: 'John Doe',
+      url: 'http://www.example.com',
+      likes: 12,
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .set('Authorization', `bearer ${token}`)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  })
+
   test('when request doesn\'t include likes', async () => {
-    const id = await getOneUserId()
     const newBlog = {
       title: 'prueba',
       author: 'prueba',
       url:'prueba',
-      userId: id
     }
 
     const response = await api
       .post('/api/blogs')
       .send(newBlog)
+      .set('Authorization', `bearer ${token}`)
       .expect(201)
       .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
     expect(response.body.likes).toBe(0)
   })
 
   test('when request doesn\'t include title or url', async () => {
-    const id =  await getOneUserId()
     const newBlog ={
       author: 'prueba',
       likes: 24,
-      userId: id
     }
 
     await api
       .post('/api/blogs')
       .send(newBlog)
+      .set('Authorization', `bearer ${token}`)
       .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
   })
 })
 
 describe('delete blogs from database', () => {
-  test('delete existing blog', async () => {
+  test('cannot be deleted if the blog was not created by the same user', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
     await api
-      .delete('/api/blogs/this is blog 1')
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .set('Authorization', `bearer ${noBlogsToken}`)
+      .expect(400)
+  })
+
+  test('can be deleted if the blog was created by the same user', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .set('Authorization', `bearer ${token}`)
       .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
+
+    const blogsIds = blogsAtEnd.map((b) => b.id)
+    expect(blogsIds).not.toContain(blogToDelete.id)
   })
 })
 
 describe('update blogs from database', () => {
-  test('update exising blog', async () => {
-    await api
-      .put('/api/blogs/this is blog 1')
-      .expect(204)
+  test('can have more likes added', async () => {
+    const allBlogsinDb = await helper.blogsInDb()
+    const blogToBeUpdated = allBlogsinDb[0]
+
+    const updatedData = {
+      likes: 100,
+    }
+
+    const updatedBlog = await api
+      .put(`/api/blogs/${blogToBeUpdated.id}`)
+      .send(updatedData)
+      .set('Authorization', `bearer ${token}`)
+      .expect(200)
+
+    expect(updatedBlog.body.likes).toBe(updatedData.likes)
   })
 })
 
