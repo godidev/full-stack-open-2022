f92de96f5adca755e3a27da81f5f46b2f06ef771
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-02-02 17:56:17 +0100

    feat: add testing route for reset

--- part4/app.js
+++ part4/app.js
@@ -26,5 +26,10 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 
 module.exports = app
