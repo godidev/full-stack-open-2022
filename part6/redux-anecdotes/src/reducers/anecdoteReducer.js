3791baf164c6683b028b9c499fbed04feae699f4
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-02-05 17:44:40 +0100

    feat: votes saved to store

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,26 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch(action.type) {
+    case 'VOTE':
+      const {id} = action.payload
+      return state.map(anecdote => {
+        return anecdote.id === id 
+        ? {...anecdote, votes: anecdote.votes + 1}
+        : anecdote
+      })
+    default:
+      return state
+  }
+}
+
+export const vote = id => {
+  return {
+    type: 'VOTE',
+    payload: {
+      id,
+    }
+  }
 }
 
 export default reducer
