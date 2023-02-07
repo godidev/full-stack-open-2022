77e4eda51115d0628bd989abc06198b47002ff70
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-02-07 19:40:18 +0100

    feat: get anecdotes from backend

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import {store} from './store'
+import {getAll} from './services/anecdotes'
+import {appendAnecdote} from './reducers/anecdoteReducer'
+
+getAll().then(anecdotes => {
+  anecdotes.forEach(anecdote => {
+    store.dispatch(appendAnecdote(anecdote))
+  })
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
