f4e9d03d0a98598db8b41cadebe02291da5efa56
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-02-11 13:38:27 +0100

    feat: add routes and links to main pages

--- part7/routed-anecdotes-main/src/index.js
+++ part7/routed-anecdotes-main/src/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+    BrowserRouter as Router,
+    Routes, Route, Link
+  } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <Router>
+        <App />
+    </Router>
+)
