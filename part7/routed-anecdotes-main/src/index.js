92ca835420dc513b9e513363186f0a1b5a3b6145
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-02-11 16:27:32 +0100

    fix: remove unused imports from index

--- part7/routed-anecdotes-main/src/index.js
+++ part7/routed-anecdotes-main/src/index.js
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import {
-    BrowserRouter as Router,
-    Routes, Route, Link
+    BrowserRouter as Router
   } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
